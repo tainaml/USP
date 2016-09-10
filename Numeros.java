3a7459181a83cf3078ec4a1cdeb3baa76dfcef55
tainaml <tainaml@live.ca>
2016-09-10 20:02:02 -0300

    Lista USP
    Questão 04

---
+++ Numeros.java
@@ -0,0 +1,43 @@
+//Lista USP - Questão 04
+
+
+public class Numeros {
+	
+	
+	private int x;
+	private int y;
+	
+	Numeros(int x, int y) {
+		
+		this.x = x;
+		this.y = y;
+		
+	}
+	
+	public void  CalculoPotencia() {
+		
+		System.out.println( Math.pow (x,y));
+		
+		
+	}
+	
+
+}
+
+
+//main
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		
+		Numeros n1 = new Numeros(-3,2);
+		
+		n1.CalculoPotencia();
+
+	}
+
+}
+
